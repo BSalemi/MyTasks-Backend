1f6784c64ca5c9b89b975a6f65240954c17ce715
Brian <briandsalemi@gmail.com>
2020-03-24 08:12:08 -0400

    added validations for presence and uniqueness to username, email_address, and password attributes in User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
+    validates(:username, :presence => true, :uniqueness => true)
+    validates(:email_address, :presence => true, :uniqueness => true)
+    validates(:password, :presence => true)
+
     has_many :tasks 
 end
