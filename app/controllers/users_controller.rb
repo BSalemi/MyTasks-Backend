80ae57b7f233ee945708c599ef0506346d358794
Brian <briandsalemi@gmail.com>
2020-04-01 16:34:02 -0400

    added create method and private strong params method for user controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+
+    def create
+        user = User.new(email_address: params[:emailAddress], username: params[:username], password: params[:password], password_confirmation: params[:passwordConfirm])
+        
+        if user.valid? 
+            user.save
+            render json: user, :include => {
+                tasks: {
+                    except: [:created_at, :updated_at] 
+                },
+            }, except: [:created_at, :updated_at]
+        else
+            render json: {message: "An error occurred. Please check that all fields are entered correctly."}
+    end
+
+
+    private 
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    end 
 end
