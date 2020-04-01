5215760da81df158d2e865afa8b86bbfc12b5c80
Brian <briandsalemi@gmail.com>
2020-04-01 17:00:03 -0400

    created index route for users controller added index route to users in config/routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,14 @@
 class UsersController < ApplicationController
 
+    def index 
+        users = User.all 
+
+        render json: users, except: [:created_at, :updated_at]
+    end 
+
     def create
         user = User.new(email_address: params[:emailAddress], username: params[:username], password: params[:password], password_confirmation: params[:passwordConfirm])
-        
+
         if user.valid? 
             user.save
             render json: user, :include => {
