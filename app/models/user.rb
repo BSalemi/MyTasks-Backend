3a1cb46ebb8a4527cf9e5a09f387723538ce71fc
Brian <briandsalemi@gmail.com>
2020-04-01 16:52:09 -0400

    added create method for user controll to config/routes. added validation checks for lengths to password and username attribute for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
-    validates(:username, :presence => true, :uniqueness => true)
+    validates(:username, :presence => true, :uniqueness => true, :length => {:within => 6..12, :wrong_length => "Username must be between six and twelve characters long."} )
+
     validates(:email_address, :presence => true, :uniqueness => true)
-    validates(:password, :presence => true)
+
+    validates(:password, :presence => true,  :length => {:within => 8..16, :wrong_length => "Passwords must be between eight and sixteen characters long."})
 
     validates_confirmation_of :password, :message => "Passwords do not match"
-        
+
     validates_presence_of :password_confirmation
 
     has_many :tasks
