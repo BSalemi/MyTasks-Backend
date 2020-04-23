7d74248f0b7500b08dcfb3531b02b2c14af76e47
Brian <briandsalemi@gmail.com>
2020-04-23 11:08:34 -0400

    added create route for TasksController to config/routes. added create method to TasksController with paramters passed in from TaskForm component in the frontend

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,4 +5,10 @@ class TasksController < ApplicationController
 
         render json: tasks, except: [:created_at, :updated_at]
     end
+
+    def create
+        task = Task.create(to_do: params[:to_do], user_id: params[:user_id])
+
+        render json: task, except: [:created_at, :updated_at]
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :tasks, only: [:index]
+  resources :tasks, only: [:create, :index]
   resources :users, only: [:create, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
